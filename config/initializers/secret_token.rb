a4da75fc87eb38a4fc11b7c44c80dba0aa6bd236
gpg <gpg@debian.monterosa.co.uk>
2011-05-12 19:03:38 -0400

    std version

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Perftest::Application.config.secret_token = 'e752c720d296aa5b9c2ba31faafdba5e35b8d4d6f5bcab3a30aeb87bb28a9816981362484bce5ba2de4a8d2e8ea41310cd733f128e4c2adafea50750015607cf'
---
+++ db/migrate/20110512212418_create_users.rb
@@ -0,0 +1,16 @@
+class CreateUsers < ActiveRecord::Migration
+  def self.up
+    create_table :users do |t|
+      t.string :name
+      t.string :gender
+      t.string :location
+      t.integer :age
+
+      t.timestamps
+    end
+  end
+
+  def self.down
+    drop_table :users
+  end
+end
